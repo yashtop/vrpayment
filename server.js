12aefef74d3df744e77fb228d8759300341d653f
suyash <yashtop@gmail.com>
2018-01-24 13:13:45 +0530

    add addition condition

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ var app = express();
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Welcome to vr payment gateway please try http://vrpayment.fr.openode.io/api/payment?transactionID=1236666&amount=30&mobilenumber=9716841138&email=abc@email.com');
+  res.send('Welcome to vr payment gateway please try http://vrpayment.fr.openode.io/api/payment?transactionID=1236666&amount=30&mobilenumber=9716841138&email=abc@email.com&merchantId=2121');
 });
 app.get('/api/payment', function(req, res) {
   var transactionID = req.param('transactionID');
   var amount = req.param('amount');
   var mobilenumber = req.param('mobilenumber');
   var email = req.param('email');
-  if(!transactionID && !amount && !mobilenumber && !email){
+  var merchantId = req.param('merchantId');
+  if(!transactionID && !amount && !mobilenumber && !email && !merchantId){
 	var json = {"status":"0","message":"required complete param(s)"};
 	
   }else{
-	var orderID = Date.now();
-	var json = {"status":"1","message":"payment has been made successfully","orderid":orderID};
+	if(amount>1000){
+		var json = {"status":"2","message":"you don't have balance to purchase."};
+	}else{
+		var orderID = Date.now();
+		var json = {"status":"1","message":"Payment has been made successfully","orderid":orderID};
+	}
   }
   res.send(json);
   
@@ -23,5 +28,3 @@ app.get('/api/payment', function(req, res) {
 // Listen
 var port = 8080;
 app.listen(port);
-
-console.log('Listening on localhost:'+ port);
