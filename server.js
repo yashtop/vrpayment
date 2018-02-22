1945fe846c685d4d19df84b498f21056c69e877b
suyash mishra <suyash.a.mishra@accenture.com>
2018-02-22 15:01:57 +0530

    public folder added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // Routes
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.send('Welcome to vr payment gateway please try http://vrpayment.fr.openode.io/api/payment?transactionID=1236666&amount=30&mobilenumber=9716841138&email=abc@email.com&merchantId=2121');
 });
@@ -14,7 +14,7 @@ app.get('/api/payment', function(req, res) {
   var merchantId = req.param('merchantId');
   if(!transactionID || !amount || !mobilenumber || !email || !merchantId){
 	var json = {"status":"0","message":"required complete param(s)"};
-	
+
   }else{
 	if(amount>1000){
 		var json = {"status":"2","message":"you don't have balance to purchase."};
@@ -24,7 +24,7 @@ app.get('/api/payment', function(req, res) {
 	}
   }
   res.send(json);
-  
+
 });
 // Listen
 var port = 8080;
